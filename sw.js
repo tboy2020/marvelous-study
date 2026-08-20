9ce61446c1541450d5f6cc099947d2ae4a6a00b4
tboy2020 <67811855+tboy2020@users.noreply.github.com>
2026-08-20 12:48:37 -0400

    Bump SW cache to v4 to force the fixed app to load

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'studylab-v3';
+var CACHE = 'studylab-v4';
 var ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', function(e){
