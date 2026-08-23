12965b3adf02843021476653396def1c2a0bd5be
tboy2020 <67811855+tboy2020@users.noreply.github.com>
2026-08-23 17:40:19 -0400

    Bump cache to v5 so the fixed backup loads

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'studylab-v4';
+var CACHE = 'studylab-v5';
 var ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', function(e){
