aa810d3a822d933165f20fcd21bd00608dcfa60b
tboy2020 <67811855+tboy2020@users.noreply.github.com>
2026-08-23 18:21:33 -0400

    SW v7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'studylab-v6';
+var CACHE = 'studylab-v7';
 var ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', function(e){
